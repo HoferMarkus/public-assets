897395c86026213af00490794992f93c5440d030
HOF <Markus.Hofer@combeenation.com>
2023-10-06 10:49:51 +0200

    some more tries...

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,6 +1,6 @@
-import "https://cdn.jsdelivr.net/npm/@babylonjs/gui@6.23.0/2D/math2D.js";
+import "https://cdn.jsdelivr.net/npm/@babylonjs/gui@6.23.0/2D/math2D.js/+esm";
 import { EngineStore } from "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/+esm";
-import "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/Debug/debugLayer.js";
+import "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/Debug/debugLayer.js/+esm";
 import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.23.0/+esm";
 
 console.log(EngineStore);
