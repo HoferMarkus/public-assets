7ecc0580d1b6c27f54a03ec02f1752d778699d16
HOF <Markus.Hofer@combeenation.com>
2023-10-06 10:12:25 +0200

    remove gui import

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,3 +1,3 @@
-import "https://cdn.jsdelivr.net/npm/@babylonjs/gui@6.23.0/+esm";
-import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.23.0/+esm";
+import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.22.0/+esm";
+
 window.myInspector = babylonjsinspector;
