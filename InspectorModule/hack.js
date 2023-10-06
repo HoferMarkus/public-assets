790062581a9c677c0289e5f6417e4e26d5380457
HOF <Markus.Hofer@combeenation.com>
2023-10-06 09:59:30 +0200

    update module import

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,3 +1,3 @@
-import "https://cdn.jsdelivr.net/npm/@babylonjs/gui-editor@6.23.0/+esm";
+import "https://cdn.jsdelivr.net/npm/@babylonjs/gui@6.23.0/+esm";
 import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.23.0/+esm";
 window.myInspector = babylonjsinspector;
