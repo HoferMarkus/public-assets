ee21d9fa4d2fdc106e0657ca5f899962c122eaff
HOF <Markus.Hofer@combeenation.com>
2023-10-06 10:26:01 +0200

    add logs

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,4 +1,7 @@
 import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.22.0/+esm";
-import { EngineStore } from "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/+esm";
+import { EngineStore } from "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.22.0/+esm";
+
+console.log(EngineStore);
+console.log(babylonjsinspector);
 
 babylonjsinspector.Inspector.show(EngineStore._LastCreatedScene);
