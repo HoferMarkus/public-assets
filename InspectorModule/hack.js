5ef23c8bf08b30650109e0b711a74f821870db4c
HOF <Markus.Hofer@combeenation.com>
2023-10-06 10:46:02 +0200

    update math 2D import

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,3 +1,4 @@
+import "https://cdn.jsdelivr.net/npm/@babylonjs/gui@6.23.0/2D/math2D.js";
 import { EngineStore } from "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/+esm";
 import "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/Debug/debugLayer.js";
 import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.23.0/+esm";
