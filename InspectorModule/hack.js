d7b3e902f72bf52eb4af68d358387f65b0a697c6
HOF <Markus.Hofer@combeenation.com>
2023-10-06 09:56:54 +0200

    add gui editor import

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,2 +1,3 @@
+import "https://cdn.jsdelivr.net/npm/@babylonjs/gui-editor@6.23.0/+esm";
 import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.23.0/+esm";
 window.myInspector = babylonjsinspector;
