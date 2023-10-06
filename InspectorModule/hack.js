41f5c91abd268ad8d9c1636df816667d896e1f92
HOF <Markus.Hofer@combeenation.com>
2023-10-06 10:23:24 +0200

    call inspector show immediately

--- InspectorModule/hack.js
+++ InspectorModule/hack.js
@@ -1,3 +1,4 @@
 import babylonjsinspector from "https://cdn.jsdelivr.net/npm/@babylonjs/inspector@6.22.0/+esm";
+import { EngineStore } from "https://cdn.jsdelivr.net/npm/@babylonjs/core@6.23.0/+esm";
 
-window.myInspector = babylonjsinspector;
+babylonjsinspector.Inspector.show(EngineStore._LastCreatedScene);
